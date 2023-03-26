5b3e60ea91037e7c07480b97fc25f39b6bea4a66
Shimakuro <mineminou122@gmail.com>
2023-03-26 18:35:06 +0200

    gfdfgdfgd

--- extension-firefox/PassSave.js
+++ extension-firefox/PassSave.js
@@ -1,12 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const infoDisplay = document.getElementById('info');    
+const inputText = document.querySelectorAll('input[type="text"]')
+const inputPassword = document.querySelectorAll('input[type="password"]')
+const inputEmail = document.querySelectorAll('input[type="email"]');
 
-    window.addEventListener('DOMContentLoaded', function () {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {}, function (result) {
-                infoDisplay.innerHTML = result
-            });
-        });
-    });
 
+
+document.querySelectorAll("button[type=submit]").forEach(i => {
+    i.addEventListener("click", event => {
+        inputText.forEach(j => {
+            localStorage.setItem(j.value, j.value);
+            console.log(localStorage.getItem(j.value))
+        })
+        inputPassword.forEach(k => {
+            localStorage.setItem(k.value, k.value);
+            console.log(localStorage.getItem(k.value))
+        })
+        inputEmail.forEach(l => {
+            localStorage.setItem(l.value, l.value);
+            console.log(localStorage.getItem(l.value))
+        })
+    })
 });
